42b9ee3ee0aa731ae39620c994dd624f1e198ccb
lookrecius <bragin.boris@ya.ru>
2019-06-18 09:36:06 +0300

    Второй день воркшопа \Users.js - серверный как и \Posts.js

---
+++ src/components/ErrorMessage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import img from '../error.png';
+
+
+let ErrorMessage = () => {
+    return (
+            <>
+            <img src={img} alt="error"></img>
+            </>
+    )
+}
+
+
+export default ErrorMessage;
